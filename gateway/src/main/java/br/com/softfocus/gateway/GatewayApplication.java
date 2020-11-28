e3d22e1a838c511efc2a98ba569cce6500451bee
Vinícius Ferrarini <viniciusferrarini@outlook.com>
2020-11-28 11:57:45 -0300

    gateway

---
+++ gateway/src/main/java/br/com/softfocus/gateway/GatewayApplication.java
@@ -0,0 +1,13 @@
+package br.com.softfocus.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+}
--- produtos/src/main/java/br/com/softfocus/produtos/api/ProdutoApi.java
+++ produtos/src/main/java/br/com/softfocus/produtos/api/ProdutoApi.java
@@ -1,141 +1,49 @@
 package br.com.softfocus.produtos.api;
 
 
-import br.com.softfocus.produtos.dto.ProdutoPersistDto;
-import br.com.softfocus.produtos.dto.ProdutoUpdateDto;
-import br.com.softfocus.produtos.event.ProdutoPersistEvent;
-import br.com.softfocus.produtos.exception.ProdutoNotFoundException;
 import br.com.softfocus.produtos.model.Produto;
 import br.com.softfocus.produtos.repository.ProdutoRepository;
-import br.com.softfocus.produtos.util.JsonUtil;
-import org.modelmapper.ModelMapper;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.context.ApplicationEventPublisher;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.hateoas.CollectionModel;
-import org.springframework.hateoas.EntityModel;
-import org.springframework.hateoas.IanaLinkRelations;
-import org.springframework.http.ResponseEntity;
-import org.springframework.lang.NonNull;
-import org.springframework.security.access.prepost.PreAuthorize;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
-import java.util.stream.Collectors;
+import java.util.List;
+import java.util.Optional;
 
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
-@RequestMapping
+@RequestMapping("")
 public class ProdutoApi {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(ProdutoApi.class);
-
-    private final ProdutoRepository repository;
-
-    private final ProdutoModelAssembler produtoModelAssembler;
-
-    private final ModelMapper modelMapper;
-
-    private final ApplicationEventPublisher applicationEventPublisher;
-
-    public ProdutoApi(
-            ProdutoRepository repository,
-            ProdutoModelAssembler produtoModelAssembler,
-            ModelMapper modelMapper,
-            ApplicationEventPublisher applicationEventPublisher) {
-        this.repository = repository;
-        this.produtoModelAssembler = produtoModelAssembler;
-        this.modelMapper = modelMapper;
-        this.applicationEventPublisher = applicationEventPublisher;
-    }
+    @Autowired private ProdutoRepository repository;
+    @Autowired private JmsTemplate jmsTemplate;
+    @Autowired private ObjectMapper objectMapper;
 
     @GetMapping
-    public CollectionModel<EntityModel<Produto>> all(
-            @NonNull @RequestParam(value = "size", defaultValue = "10", required = false) Integer size,
-            @NonNull @RequestParam(value = "page", defaultValue = "0", required = false) Integer page) {
-        LOGGER.info("Buscando os produtos: size = {}, page = {}", size, page);
-
-        var pageable = PageRequest.of(page, size);
-
-        var produtos = repository.findAll(pageable).stream()
-                .map(produtoModelAssembler::toModel)
-                .collect(Collectors.toList());
-
-        return CollectionModel.of(produtos, linkTo(methodOn(ProdutoApi.class).all(size, page))
-                .withSelfRel());
+    public List<Produto> all() {
+        return repository.findAll();
     }
 
-    @PreAuthorize("hasAuthority('SCOPE_TEST')")
     @GetMapping("{id}")
-    public EntityModel<Produto> one(@NonNull @PathVariable("id") Long id) {
-        LOGGER.info("Buscando o produto: id = {}", id);
-
-        var produto = repository.findById(id)
-                .orElseThrow(() -> new ProdutoNotFoundException(id));
-        return produtoModelAssembler.toModel(produto);
+    public Optional<Produto> one(@PathVariable("id") Long id) {
+        return repository.findById(id);
     }
 
     @PostMapping
-    public ResponseEntity<EntityModel<Produto>> newProduto(@NonNull @Valid @RequestBody ProdutoPersistDto produtoPersist) {
-        LOGGER.info("Inserindo o produto: {}", produtoPersist);
+    public Produto insert(@RequestBody Produto produto) throws JsonProcessingException {
+        repository.save(produto);
 
-        var produto = modelMapper.map(produtoPersist, Produto.class);
-        var entityModel = produtoModelAssembler.toModel(repository.save(produto));
+        var produtoJson = objectMapper.writeValueAsString(produto);
+        jmsTemplate.convertAndSend("queue.produto.insert", produtoJson);
 
-        applicationEventPublisher.publishEvent(new ProdutoPersistEvent(this, produto));
-
-        return ResponseEntity
-                .created(entityModel.getRequiredLink(IanaLinkRelations.SELF).toUri())
-                .body(entityModel);
-    }
-
-    @PutMapping("{id}")
-    public ResponseEntity<EntityModel<Produto>> update(
-            @NonNull @Valid @RequestBody ProdutoPersistDto produtoPersist,
-            @NonNull @PathVariable Long id) {
-        LOGGER.info("Alterando o produto: id = {}, {}", id, produtoPersist);
-
-        var produto = repository.findById(id)
-                .orElseThrow(() -> new ProdutoNotFoundException(id));
-
-        modelMapper.map(produtoPersist, produto);
-
-        var entityModel = produtoModelAssembler.toModel(repository.save(produto));
-
-        applicationEventPublisher.publishEvent(new ProdutoPersistEvent(this, produto));
-
-        return ResponseEntity.ok(entityModel);
+        return produto;
     }
 
     @DeleteMapping("{id}")
-    public ResponseEntity<?> delete(@NonNull @PathVariable Long id) {
-        LOGGER.info("Excluindo o produto: id = {}", id);
-
-        if (repository.existsById(id)) {
-            repository.deleteById(id);
-            return ResponseEntity.noContent().build();
-        }
-        throw new ProdutoNotFoundException(id);
-    }
-
-    @PatchMapping("{id}")
-    public ResponseEntity<?> updateProduto(
-            @NonNull @PathVariable("id") Long id,
-            @Valid @RequestBody ProdutoUpdateDto produtoUpdateDto) {
-        LOGGER.info("Alerando o produto: id = {}, {}", id, produtoUpdateDto);
-
-        var produto = repository.findById(id)
-                .orElseThrow(() -> new ProdutoNotFoundException(id));
-
-        JsonUtil.changeIfPresent(produtoUpdateDto.getDescricao(), produto::setDescricao);
-        JsonUtil.changeIfPresent(produtoUpdateDto.getValor(), produto::setValor);
-
-        var entityModel = produtoModelAssembler.toModel(repository.save(produto));
-
-        return ResponseEntity.ok(entityModel);
+    public void delete(@PathVariable("id") Long id) {
+        repository.deleteById(id);
     }
 
 }
