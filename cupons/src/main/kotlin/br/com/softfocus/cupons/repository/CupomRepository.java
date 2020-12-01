5e656395dfd6ef15bcbed3f9982110b4d3077401
Vinícius Ferrarini <viniciusferrarini@outlook.com>
2020-12-01 17:20:38 -0300

    Vendas e cupons

---
+++ cupons/src/main/kotlin/br/com/softfocus/cupons/repository/CupomRepository.java
@@ -0,0 +1,7 @@
+package br.com.softfocus.cupons.repository;
+
+import br.com.softfocus.cupons.model.Cupom;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CupomRepository extends CrudRepository<Cupom, Long> {
+}
